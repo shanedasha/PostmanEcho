a9ada506c4889c9e48771784562643d0414f9469
shanedasha <chirkova_99@inbox.ru>
2021-12-11 17:41:24 +0300

    Initial commit

---
+++ src/test/java/ru/netology/PostmanTest.java
@@ -0,0 +1,22 @@
+package ru.netology;
+
+import org.junit.jupiter.api.Test;
+
+import static io.restassured.RestAssured.given;
+import static org.hamcrest.Matchers.equalTo;
+
+public class PostmanTest {
+    @Test
+    void shouldRPostRequest() {
+        given()
+                .baseUri("https://postman-echo.com")
+                .body("some data")
+                // Выполняемые действия
+                .when()
+                .post("/post")
+                // Проверки
+                .then()
+                .statusCode(200)
+                .body("data", equalTo("some data"));
+    }
+}
